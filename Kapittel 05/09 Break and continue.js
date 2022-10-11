1860373d729a9bbd76cb6a5d09fd3eb39d6c22d8
Hjaldrgud <lauvhjell@gmail.com>
2022-10-11 23:57:48 +0200

    Ferdig med kapittel 5, starta kapittel 6

---
+++ Kapittel 05/09 Break and continue.js
@@ -0,0 +1,27 @@
+
+        let $cars = [
+            {
+                model: "Golf",
+                make: "Volkswagen",
+                year: 1999,
+                color: "black",
+            },
+            {
+                model: "Picanto",
+                make: "KIA",
+                year: 2020,
+                color: "red",
+            },
+            {
+                model: "Peugeot",
+                make: "208",
+                year: 2021,
+                color: "black",
+            },
+            {
+                model: "Fiat",
+                make: "Punto",
+                year: 2020,
+                color: "black",
+            }
+        ];
